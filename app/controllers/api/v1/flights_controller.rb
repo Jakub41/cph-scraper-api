280528edcc04b09a2baac33c7556d9b24b06d950
jakub41 <lemiszewski@gmx.com>
2018-10-16 17:19:10 +0200

    Scraper service modified

--- app/controllers/api/v1/flights_controller.rb
+++ app/controllers/api/v1/flights_controller.rb
@@ -4,22 +4,22 @@ module Api::V1
     before_action :filter_departures, only: :index
 
     def index
-      arrivals = @arrivals.map(&:in_hash)
-      departures = @departures.map(&:in_hash)
-      data = {}
-      data[:arrivals] = arrivals if arrivals.present?
+      arrivals          = @arrivals.map(&:in_hash)
+      departures        = @departures.map(&:in_hash)
+      data              = {}
+      data[:arrivals]   = arrivals if arrivals.present?
       data[:departures] = departures if departures.present?
 
-      render json: data.to_json
-    end
-
-    def show
-      #render json: @arrivals
-      #render json: @departures
+      render json: data.to_json, status: :ok
     end
 
     def destroy_all
+      Arrival.all.delete_all
+      Departure.all.delete_all
+      render json: 'All data deleted.', status: :ok
     end
+
+    private
 
     def filter_arrivals
       @arrivals = if params[:type].present? && params[:type] != 'arrival'
@@ -28,13 +28,13 @@ module Api::V1
                     start_date = Date.parse(params[:start_date]) rescue ''
                     end_date   = Date.parse(params[:end_date]) rescue ''
                     if start_date.present? && end_date.present?
-                      Arrival.between(start_date, end_date)
+                      Arrival.between(start_date, end_date).order(:date)
                     elsif start_date.present?
-                      Arrival.from_date(start_date)
+                      Arrival.from_date(start_date).order(:date)
                     elsif end_date.present?
-                      Arrival.till_date(end_date)
+                      Arrival.till_date(end_date).order(:date)
                     else
-                      Arrival.all
+                      Arrival.all.order(:date)
                     end
                   end
     end
@@ -46,13 +46,13 @@ module Api::V1
                       start_date   = Date.parse(params[:start_date]) rescue ''
                       end_date     = Date.parse(params[:end_date]) rescue ''
                       if start_date.present? && end_date.present?
-                        Departure.between(start_date, end_date)
+                        Departure.between(start_date, end_date).order(:date)
                       elsif start_date.present?
-                        Departure.from_date(start_date)
+                        Departure.from_date(start_date).order(:date)
                       elsif end_date.present?
-                        Departure.till_date(end_date)
+                        Departure.till_date(end_date).order(:date)
                       else
-                        Departure.all
+                        Departure.all.order(:date)
                       end
                     end
     end
