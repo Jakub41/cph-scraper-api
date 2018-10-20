935c1cf3a1e9cf7729715c709c2ac4b129e6882a
jakub41 <lemiszewski@gmx.com>
2018-10-20 17:18:27 +0200

    Added seeds for testing

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,5 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Arrival.create!(time: "10:00", date: "2018-10-14", airline: "airline1", arriving_from: "loc1", flight_no: "ABCD", gate: "g1", terminal: "t1", status: "OK")
+Departure.create!(time: "11:00", date: "2018-10-14", airline: "airline2", destination: "loc2", flight_no: "XYZ", gate: "g2", terminal: "t2", status: "OK")
