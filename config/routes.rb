3152ccc9c0f654355966a14c83e1b13e04dcc993
jakub41 <lemiszewski@gmx.com>
2018-10-21 16:25:47 +0200

    Added autoload for lib & bug fix

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   namespace :api, defaults: { format: :json } do
     namespace :v1 do
       resources :flights, only: [:index, :destroy_all]
+
       get 'flights/:type', to: 'flights#filter'
     end
   end
