bf9087078f6f6020031025ac7c9390cb02cf14d9
jakub41 <lemiszewski@gmx.com>
2018-10-21 15:34:17 +0200

    Refactored the models

--- app/models/arrival.rb
+++ app/models/arrival.rb
@@ -1,10 +1,6 @@
 class Arrival < ApplicationRecord
   CPH_URL = 'https://www.cph.dk/en/flight-information/arrivals'.freeze
 
-  scope :between, ->(start_date, end_date) { where(date: start_date..end_date) }
-  scope :from_date, ->(start_date) { where('date >= ?', start_date) }
-  scope :till_date, ->(end_date) { where('date <= ?', end_date) }
-
   def in_hash
     {
         time: time || '',
--- app/models/departure.rb
+++ app/models/departure.rb
@@ -1,10 +1,6 @@
 class Departure < ApplicationRecord
   CPH_URL = 'https://www.cph.dk/en/flight-information/departures'.freeze
 
-  scope :between, ->(start_date, end_date) { where(date: start_date..end_date) }
-  scope :from_date, ->(start_date) { where('date >= ?', start_date) }
-  scope :till_date, ->(end_date) { where('date <= ?', end_date) }
-
   def in_hash
     {
         time: time || '',
