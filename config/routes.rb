d2b8b45d3ae2e612f4e78ecc6d7abf18b397e88e
jakub41 <lemiszewski@gmx.com>
2018-10-21 15:31:31 +0200

    Added Get to route

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   namespace :api, defaults: { format: :json } do
     namespace :v1 do
       resources :flights, only: [:index, :destroy_all]
+      get 'flights/:type', to: 'flights#filter'
     end
   end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
